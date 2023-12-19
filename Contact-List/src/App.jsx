44930051ef570ad6b67dc252ebb9ac773b115751
ZI FANG <fangzi@ZIs-MacBook-Pro.local>
2023-12-18 20:40:09 -0500

    initial commit

---
+++ Contact-List/src/App.jsx
@@ -0,0 +1,18 @@
+
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+
+import { useState } from 'react'
+import './App.css'
+import ContactList from './components/ContactList.jsx'
+
+function App() {
+
+  return (
+    <>
+      <ContactList />
+    </>
+  )
+}
+
+export default App
